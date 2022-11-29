4df980d991ebc6c23d661729b87812be2e49e101
i.shirinov <shirinovilyos6965@gmail.com>
2022-11-29 10:54:19 +0500

    Vazifa №2. While Loops 1

--- src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task2/EvenNumbersPrinter.java
@@ -2,19 +2,17 @@ package school.mjc.stage0.loops.task2;
 
 public class EvenNumbersPrinter {
     public void printEvenNumbers(int printTillInclusive) {
-        int count = 0;
-        int x = printTillInclusive;
-        while (count <= x) {
-            if (printTillInclusive % 2 == 0) {
-                System.out.println(printTillInclusive);
+        int x = 0;
+        while (x <= printTillInclusive) {
+            if (x % 2 == 0) {
+                System.out.println(x);
             }
-            printTillInclusive--;
-            count++;
+            x++;
         }
     }
 
     public static void main(String[] args) {
         EvenNumbersPrinter printer = new EvenNumbersPrinter();
-        printer.printEvenNumbers(10);
+        printer.printEvenNumbers(20);
     }
 }
